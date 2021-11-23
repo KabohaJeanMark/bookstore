fd5e212276edcafaba55dd700f4c16b6cded049d
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-23 12:56:57 +0300

    Wrap BooksList and BooksForm in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
+import BooksList from '../containers/BooksList';
+import BooksForm from '../containers/BooksForm';
+
 const App = () => (
   <div>
-    Welcome to The BookStore
+    Welcome to The BookStore App
+    <BooksList />
+    <BooksForm />
   </div>
 );
 
