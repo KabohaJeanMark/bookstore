a48dd369ba4e29aa4c8a50d5b7e2006bd9473835
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-24 05:34:06 +0300

    Refactor the action object to use property shorthand

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,12 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const createBook = (book) => ({
   type: CREATE_BOOK,
-  payload: book,
+  book,
 });
 
 const removeBook = (book) => ({
   type: REMOVE_BOOK,
-  payload: book,
+  book,
 });
 
 export {
