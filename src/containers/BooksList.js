4bbc4f9bd1af0b0aee2785b4aa17064af2700a20
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-11-25 07:22:44 +0300

    Render Category Filter in BooksList container component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,26 +3,31 @@ import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook } from '../actions';
 import store from '../reducers';
+import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = ({ books }) => {
   const handleRemoveBook = (book) => {
     store.dispatch(removeBook(book));
   };
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book) => (
-          <Book key={book.id} book={book} handleRemoveBook={() => { handleRemoveBook(book); }} />))}
-      </tbody>
-    </table>
+    <>
+      <CategoryFilter />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <Book key={book.id} book={book} handleRemoveBook={() => { handleRemoveBook(book); }} />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
